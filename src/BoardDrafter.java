1f959203040448b585a3bab413f0222f273c7940
Eques72 <adrian.zareba@op.pl>
2022-02-16 20:19:50 +0100

    fixed typo

--- src/BoardDrafter.java
+++ src/BoardDrafter.java
@@ -20,43 +20,43 @@ public class BoardDrafter {
             for(int w = 0; w < board.boardWidth; w++)
                 if(board.map.charAt((q*board.boardWidth) + w) == '#')
                 {
-                    if ((w == 0 || w+1 == board.boardWidth) && (q != 0 || q+1 != board.boardHeight)) // ||
+                    if ((w == 0 || w+1 == board.boardWidth) && (q != 0 && q+1 != board.boardHeight)) // ||
                         g.drawImage(
                                 sprites, w*board.tileSize,q*board.tileSize,
-                                w*board.tileSize+board.tileSize,w*board.tileSize+board.tileSize,
-                                20*5, 0,20,20, iO);
-                    else if ((w != 0 || w+1 != board.boardWidth) && (q == 0 || q+1 == board.boardHeight))  // --
+                                w*board.tileSize+board.tileSize,q*board.tileSize+board.tileSize,
+                                20*5, 0,20*6,20, iO);
+                    else if ((w != 0 && w+1 != board.boardWidth) && (q == 0 || q+1 == board.boardHeight))  // --
                         g.drawImage(
                                 sprites, w*board.tileSize,q*board.tileSize,
-                                w*board.tileSize+board.tileSize,w*board.tileSize+board.tileSize,
-                                20*4, 0,20,20, iO);
+                                w*board.tileSize+board.tileSize,q*board.tileSize+board.tileSize,
+                                20*4, 0,20*5,20, iO);
                     else if(q == 0 && w == 0) //top left
                         g.drawImage(
                                 sprites, w*board.tileSize,q*board.tileSize,
-                                w*board.tileSize+board.tileSize,w*board.tileSize+board.tileSize,
+                                w*board.tileSize+board.tileSize,q*board.tileSize+board.tileSize,
                                 0, 0,20,20, iO);
                     else if (q == 0 && w + 1 == board.boardWidth) //top right
                         g.drawImage(
                                 sprites, w*board.tileSize,q*board.tileSize,
-                                w*board.tileSize+board.tileSize,w*board.tileSize+board.tileSize,
-                                20, 0,20,20, iO);
+                                w*board.tileSize+board.tileSize,q*board.tileSize+board.tileSize,
+                                20, 0,20*2,20, iO);
                     else if (q +1 == board.boardHeight && w + 1 == board.boardWidth) //bottom right
                     g.drawImage(
                             sprites, w*board.tileSize,q*board.tileSize,
-                            w*board.tileSize+board.tileSize,w*board.tileSize+board.tileSize,
-                            3*20, 0,20,20, iO);
+                            w*board.tileSize+board.tileSize,q*board.tileSize+board.tileSize,
+                            3*20, 0,20*4,20, iO);
                     else //bottom left
                         g.drawImage(
                                 sprites, w*board.tileSize,q*board.tileSize,
-                                w*board.tileSize+board.tileSize,w*board.tileSize+board.tileSize,
-                                2*20, 0,20,20, iO);
+                                w*board.tileSize+board.tileSize,q*board.tileSize+board.tileSize,
+                                2*20, 0,20*3,20, iO);
                 }
                 else if(board.map.charAt((q*board.boardWidth) + w) == '%')
                 {
                     g.drawImage(
                             sprites, w*board.tileSize,q*board.tileSize,
-                            w*board.tileSize+board.tileSize,w*board.tileSize+board.tileSize,
-                            6*20, 0,20,20, iO);
+                            w*board.tileSize+board.tileSize,q*board.tileSize+board.tileSize,
+                            6*20, 0,20*7,20, iO);
                 }
     }
 
--- src/BoardEditor.java
+++ src/BoardEditor.java
@@ -1,2 +1,19 @@
 public class BoardEditor {
+
+    Board board;
+
+    public BoardEditor(Board b)
+    {
+        board = b;
+
+    }
+
+    public void move_down()
+    {
+//need to create blocks class, those blocks will keep their x,y. based on changing coordinates,
+// they will be placed on board
+    }
+
+    public void rotate(){}
+
 }
